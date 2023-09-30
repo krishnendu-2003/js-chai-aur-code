39dc2969ac72c48821fbdc4341288e7ed0dacc42
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-09-30 20:03:18 +0000

    changesmade

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert("Hello") / we are using node js not browser
+
+console.log(3+3);
+console.log("Krish");
+
+
+let name= "krish"
+let age= 20
+let isLoggedIn= false
+
+// number => 2 to the power of 53  (range)
+// bigint // if the number is extremely big then we use bigint
+//string
+// boolean => true/false
+// nullstandalone value
+// undefined 
+// symbol => uniqueness
+
+// object
+
+console.log(typeof "klhasf") 
+console.log(typeof null); //type is object
+console.log(typeof undefined); // type is undefined
