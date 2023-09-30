1fdf998dc3f2c50c394b7cbe23993413ef3fdab7
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-09-30 19:51:28 +0000

    Changes made

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1445544  //constant values cannot be changed
+let accoundEmail = "skrishnendu115@gmail.com"  //variables can be changed
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2  //not allowed
+
+accoundEmail = "khjkshf.com"
+accountCity="Kolkata"
+
+// console.log(accountId);
+// console.log(accoundEmail);
+// console.log(accountCity);
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/ 
+
+console.table([accountId,accoundEmail,accountCity,accountPassword,accountState]) //this will print all the datas in a tabular manner 
