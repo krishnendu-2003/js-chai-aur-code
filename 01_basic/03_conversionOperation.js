7f8497adb02a63a5c395ba3ea9ce664ebbc9609f
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-09-30 20:19:34 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+let age= "34"
+let clas="sdlkfh"
+
+console.log(typeof score);
+console.log(typeof(age));
+
+//converting a string to number
+let valueInNumber = Number(age);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let NumericValueOfclas = Number(clas)
+console.log(typeof(NumericValueOfclas))
+console.log(NumericValueOfclas);  //though it has been converted to Number but it is Not a number
+
+
+let bool1= true
+let bool2= false
+console.log(Number(bool1));
+console.log(Number(bool2));
