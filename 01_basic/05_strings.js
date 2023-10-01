84f742d3f04bf5db44d4c2279b137a21c8a65da9
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-10-01 13:36:02 +0000

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,8 @@
+const name= "Krishnendu"
+const age= 20
+
+// console.log(name + age + "samanta");  //Do not use this
+
+// console.log(`Hello my name is ${name}, and my age is ${age}`); //write in this way
+
+
