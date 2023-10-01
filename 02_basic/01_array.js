f6a85a2d111a10de4c0344729f12a6d67301da36
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-10-01 19:00:15 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,10 @@
+// Arrays
+//Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr = [0,1,2,3,4,5]
+const arr2= ["spiderman","iron man","Hulk","venom"]
+
+console.log(myArr[1]);
+
+console.log((arr2[2]));
+console.log((arr2[4]));
