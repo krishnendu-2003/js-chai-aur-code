61c355a20ff57d06396879c753af86d4270e9acf
Krishnendu Samanta <76649777+krishnendu-2003@users.noreply.github.com>
2023-10-01 15:22:29 +0000

    string

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -6,3 +6,21 @@ const age= 20
 // console.log(`Hello my name is ${name}, and my age is ${age}`); //write in this way
 
 
+const name2= new String('chaiaurcode')
+// console.log(name2[0]);
+// console.log(name2[2]);
+
+// console.log(name2.__proto__);
+
+// console.log(name2.length);
+// console.log(name2.toUpperCase());
+
+// console.log(name2.charAt(2));
+// console.log(name2.charAt(6));
+
+// console.log(name2.indexOf('d'));
+
+
+const name3= '       krishnendu        '
+console.log(name3);
+console.log(name3.trim());     //trim function removes the spaces after and before the text
